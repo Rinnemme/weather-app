eec3c6d669bb5ba75f5d05d06d7409e7dbef46f5
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-09-25 14:57:04 -0400

    set up basic interface for retrieving the data according to user-inputted number of days and zip code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './style.css'
+
+const locationInput = document.getElementById('location')
+const days = document.getElementById('days')
+const form = document.getElementById('form')
+
+async function callAPI() {
+    const weatherResponse = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=2e21eea24f714bb4a65154936231909&q=${locationInput.value}&days=${days.value}&aqi=no&alerts=no)`, {mode: 'cors'})
+    const weatherData = await weatherResponse.json()
+    console.log(weatherData)
+}
+
+function submitForm(event) {
+    event.preventDefault()
+    callAPI()
+}
+
+form.onsubmit = submitForm
+
+// new Date().getHours() will return current hours in local time
